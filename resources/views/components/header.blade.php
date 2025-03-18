1019197081a7ad89e9b2e3a80cdae5ac95249348
Blue Steam <leonardojosuan@gmail.com>
2025-03-18 12:50:08 +0700

    Refactor header component layout and Add page caption component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header class="bg-gray-50 border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-    <div class="mx-auto max-w-7xl px-2 py-2 sm:px-4">
+    <div class="max-w-7xl px-4 py-2 text-center sm:text-left">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $slot }}</h1>
     </div>
 </header>
---
+++ resources/views/components/page-caption.blade.php
@@ -0,0 +1,5 @@
+<div class="p-4 bg-gray-50 dark:bg-gray-800">
+    <h2 class="font-semibold text-justify rtl:text-right text-gray-500 dark:text-gray-400">
+        {{ $slot }}
+    </h2>
+</div>
