a194fb5f484075cce96081c460482e435f7ab74b
Blue Steam <leonardojosuan@gmail.com>
2025-03-23 23:24:26 +0700

    Add toast component

---
+++ resources/views/components/toast.blade.php
@@ -0,0 +1,48 @@
+@props(['type' => 'success', 'message' => ''])
+
+@if ($message)
+    <div id="toast_{{ $type }}"
+        class="fixed top-10/12 right-1/2 translate-1/2 z-40 flex items-center w-full max-w-xs p-2 mb-4 text-gray-500 bg-white rounded-lg border border-gray-400 shadow-sm dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600 sm:top-24 sm:right-4 sm:translate-0"
+        role="alert">
+        <div
+            class="inline-flex items-center justify-center shrink-0 w-8 h-8 text-{{ $type == 'success' ? 'green' : 'red' }}-500 bg-{{ $type == 'success' ? 'green' : 'red' }}-100 rounded-lg dark:bg-{{ $type == 'success' ? 'green' : 'red' }}-800 dark:text-{{ $type == 'success' ? 'green' : 'red' }}-200">
+
+            @if ($type == 'success')
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 0 20 20">
+                    <path
+                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                </svg>
+            @else
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 0 20 20">
+                    <path
+                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+                </svg>
+            @endif
+
+            <span class="sr-only">Check icon</span>
+        </div>
+
+        <div class="ms-3 text-sm font-normal">{{ session('success') }}</div>
+
+        <button type="button"
+            class="ms-auto bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            
+            onclick="dismissToast('{{ $type }}')" aria-label="Close">
+            <span class="sr-only">Close</span>
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+        </button>
+    </div>
+@endif
+
+<script>
+    function dismissToast(type) {
+        const toast = document.getElementById('toast_' + type);
+        toast.style.display = 'none';
+    }
+</script>
