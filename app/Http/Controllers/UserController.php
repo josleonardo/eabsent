25d8b1880ae6a1d734c721769cbe3823983e5439
Blue Steam <leonardojosuan@gmail.com>
2025-03-23 23:31:24 +0700

    Add user creation functionality with role attach and validation and update UI

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -21,7 +24,8 @@ class UserController extends Controller
      */
     public function create()
     {
-        return view('administrators.users.create', ['pageName' => 'Add user']);
+        $roles = Role::all();
+        return view('administrators.users.create', ['pageName' => 'Add user'], compact('roles'));
     }
 
     /**
@@ -29,7 +33,65 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $currentUserId = Auth::id();
+
+        $validatedData = $request->validate([
+            'email' => 'required|email|unique:users',
+            'fullname' => 'required|string|max:255',
+            'username' => 'nullable|string|max:255|unique:users',
+            'password' => [
+                'required',
+                'string',
+                'min:8', // Minimum 8 characters
+                'regex:/[0-9]/', // Must contain a number
+                'regex:/[!-\/:-@[-`{-~]/', // Must contain a special character
+            ],
+            'nik' => 'required|string|max:16|unique:user_profiles,nik',
+            'nuptk' => 'nullable|string|max:16|unique:user_profiles,nuptk',
+            'position' => 'nullable|string|max:255',
+            'address' => 'nullable|string|max:255',
+            'phone_number' => 'nullable|string|max:20',
+            'role' => 'nullable|integer|exists:roles,id',
+            'employment_start' => 'required|date',
+            'employment_end' => 'nullable|date',
+            'active' => 'required|boolean',
+        ], [
+            'password.regex' => 'The password must contain at least one number and one special character.',
+        ]);
+
+        // Create the user
+        $user = User::create([
+            'email' => $validatedData['email'],
+            'username' => $validatedData['username'],
+            'password' => Hash::make($validatedData['password']),
+            'active' => $validatedData['active'],
+            'created_by' => $currentUserId,
+            'updated_by' => $currentUserId,
+        ]);
+
+        // Create the user profile
+        $user->profile()->create([
+            'fullname' => $validatedData['fullname'],
+            'nik' => $validatedData['nik'],
+            'nuptk' => $validatedData['nuptk'],
+            'position' => $validatedData['position'],
+            'address' => $validatedData['address'],
+            'phone_number' => $validatedData['phone_number'],
+            'employment_start' => $validatedData['employment_start'],
+            'employment_end' => $validatedData['employment_end'],
+            'active' => $validatedData['active'],
+            'created_by' => $currentUserId,
+            'updated_by' => $currentUserId,
+        ]);
+
+        // Attach the role to the user
+        $user->role()->attach($validatedData['role'], [
+            'active' => $validatedData['active'],
+            'created_by' => $currentUserId,
+            'updated_by' => $currentUserId,
+        ]);
+
+        return redirect()->route('user.index')->with('success', 'User added successfully');
     }
 
     /**
--- resources/views/administrators/users/create.blade.php
+++ resources/views/administrators/users/create.blade.php
@@ -1,17 +1,23 @@
 <x-layout>
     <x-slot:pageName>{{ $pageName }}</x-slot>
 
-    <section class="p-3 border border-gray-200 sm:p-4 dark:bg-gray-800 dark:border-gray-700">
+    <section class="p-3 space-y-6 border border-gray-200 sm:p-4 dark:bg-gray-800 dark:border-gray-700">
+        {{-- Back button --}}
         <a href="{{ route('user.index') }}"
-            class="inline-flex items-center mb-4 p-2.5 rounded-md bg-blue-600 font-semibold text-sm text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
-            <svg class="w-3.5 h-3.5 text-white me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-                <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
-                <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
+            class="inline-flex items-center p-2.5 rounded-md bg-blue-600 font-semibold text-sm text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+            <svg class="size-5 text-white me-2" aria-hidden="true" fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                <path
+                    d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
             </svg>
             Back
         </a>
-        
-        <form action="#" method="POST" class="max-w-7xl">
+
+        {{-- Create form --}}
+        <form action="{{ route('user.store') }}" method="POST" class="space-y-6 max-w-7xl">
+            @csrf
+
             <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                 <div class="w-full">
                     <label for="email" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
@@ -20,7 +26,11 @@
                     <input type="email" name="email" id="email" placeholder="user@example.com" required
                         value="{{ old('email') }}"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('email')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="w-full">
                     <label for="fullname" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
                         Full Name
@@ -28,7 +38,11 @@
                     <input type="text" name="fullname" id="fullname" placeholder="John Doe" required
                         value="{{ old('fullname') }}"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('fullname')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="w-full">
                     <label for="username" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
                         Username
@@ -36,28 +50,46 @@
                     <input type="text" name="username" id="username" placeholder="JohnDoe123"
                         value="{{ old('username') }}"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('username')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="w-full">
                     <label for="password" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
                         Password
                     </label>
                     <input type="password" name="password" id="password"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('password')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="w-full">
                     <label for="nik" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
                         NIK
                     </label>
                     <input type="text" name="nik" id="nik" placeholder="1234567890123456" required
+                        value="{{ old('nik') }}"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('nik')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="w-full">
                     <label for="nuptk" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
                         NUPTK
                     </label>
                     <input type="text" name="nuptk" id="nuptk" placeholder="2368934567893653"
+                        value="{{ old('nuptk') }}"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('nuptk')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="w-full">
                     <label for="position" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
                         Position
@@ -65,48 +97,117 @@
                     <input type="text" name="position" id="position" placeholder="Teacher"
                         value="{{ old('position') }}"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('position')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="w-full">
                     <label for="address" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
                         Current Address
                     </label>
-                    <input type="text" name="address" id="address" placeholder="Jl. Abc 1/2, Jakarta, Indonesia"
+                    <input type="text" name="address" id="address"
+                        placeholder="Jl. Abc 1/2, Jakarta, Indonesia" value="{{ old('address') }}"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('address')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div class="w-full">
-                    <label for="phoneNumber" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
+                    <label for="phone_number" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
                         Phone Number
                     </label>
-                    <input type="text" name="phoneNumber" id="phoneNumber" placeholder="081212345678"
+                    <input type="text" name="phone_number" id="phone_number" placeholder="081212345678"
+                        value="{{ old('phone_number') }}"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('phone_number')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
+
                 <div>
                     <label for="role" class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
                         Role
                     </label>
                     <select name="role" id="role"
+                        value="{{ old('role') }}"
                         class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                        <option selected="">Select role</option>
-                        <option value="TV">TV/Monitors</option>
-                        <option value="PC">PC</option>
-                        <option value="GA">Gaming/Console</option>
-                        <option value="PH">Phones</option>
+                        <option value="" selected>Select role</option>
+                        @foreach ($roles as $role)
+                            <option value="{{ $role->id }}">{{ $role->role_name }}</option>
+                        @endforeach
                     </select>
+                    @error('role')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="w-full">
+                    <label for="employment_start"
+                        class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
+                        Employment Start
+                    </label>
+                    <input type="date" name="employment_start" id="employment_start"
+                        value="{{ old('employment_start') }}"
+                        class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('employment_start')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="w-full">
+                    <label for="employment_end"
+                        class="block mb-2 text-sm/6 font-medium text-gray-900 dark:text-white">
+                        Employment End
+                    </label>
+                    <input type="date" name="employment_end" id="employment_end"
+                        value="{{ old('employment_end') }}"
+                        class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 border border-gray-200 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                    @error('employment_end')
+                        <p class="mt-2 text-sm text-red-600 dark:text-red-500"> {{ $message }}</p>
+                    @enderror
                 </div>
             </div>
 
-            <label class="mt-4 inline-flex items-center cursor-pointer">
-                <input type="checkbox" value="" class="sr-only peer" checked>
+            {{-- Active/inactive button --}}
+            <label class="inline-flex items-center cursor-pointer">
+                <input type="checkbox" name="active_checkbox" id="active_checkbox" class="sr-only peer" checked
+                    onclick="toggleActive()">
+                <input type="hidden" name="active" id="active" value="1">
                 <div
                     class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600">
                 </div>
-                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Active</span>
+                <span id="active_text" class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Active</span>
             </label>
 
+            {{-- Add data button --}}
             <button type="submit"
-                class="flex w-40 justify-center mt-6 rounded-md bg-indigo-600 p-2.5 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                class="flex w-40 items-center justify-center rounded-md bg-indigo-600 p-2.5 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <svg class="size-5 text-white me-2" aria-hidden="true" fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                    <path
+                        d="M64 80c-8.8 0-16 7.2-16 16l0 320c0 8.8 7.2 16 16 16l320 0c8.8 0 16-7.2 16-16l0-320c0-8.8-7.2-16-16-16L64 80zM0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zM200 344l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
+                </svg>
                 {{ $pageName }}
             </button>
         </form>
     </section>
 </x-layout>
+
+<script>
+    function toggleActive() {
+        const checkbox = document.getElementById('active_checkbox');
+        const hiddenInput = document.getElementById('active');
+        const activeText = document.getElementById('active_text');
+
+        if (checkbox.checked) {
+            hiddenInput.value = "1";
+            activeText.textContent = "Active";
+        } else {
+            hiddenInput.value = "0";
+            activeText.textContent = "Inactive";
+        }
+    }
+</script>
