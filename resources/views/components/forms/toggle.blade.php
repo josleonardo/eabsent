5e9417c62116e5f8439a03ddabc4934363fcc00c
Blue Steam <leonardojosuan@gmail.com>
2025-04-29 19:04:22 +0700

    Change Btn-Active to Toggle component for  boolean input handling

--- resources/views/components/forms/toggle.blade.php
+++ resources/views/components/forms/toggle.blade.php
@@ -1,10 +1,11 @@
-@props(['active' => 1])
+<label x-data="{ on: @js($checked) }" class="inline-flex items-center cursor-pointer">
+    <input type="checkbox" class="sr-only peer" @click="on = !on" {{ $checked ? 'checked' : '' }} />
 
-<label class="inline-flex items-center cursor-pointer">
-    <input type="checkbox" name="active_checkbox" id="active_checkbox" class="sr-only peer" {{ $active ? 'checked' : '' }} onclick="toggleActive()">
-    <input type="hidden" name="active" id="active" value="{{ $active }}">
+    <input type="hidden" name="{{ $name }}" :value="on ? 1 : 0">
+    
     <div
         class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600">
     </div>
-    <span id="active_text" class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">{{ $active ? 'Active' : 'Inactive' }}</span>
+
+    <span id="active_text" class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300" x-text="on ? '{{ $trueLabel }}' : '{{ $falseLabel }}'"></span>
 </label>
